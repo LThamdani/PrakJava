7e05f35e19cfa4fa4df2303b3e013c6307757716
LThamdani <lt.hamdani.x@gmail.com>
2021-05-24 16:33:11 -0700

    deklarasi variabel dan tipe data

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes{
+ public static void main(String[] args){
+
+  String namaDepan = "Muhammad";
+  String namaTengah = "Hamdani";
+  String namaBelakang ="X";
+  int usia = 19;
+  int targetTahunKuliah = 4;
+  double ipk = 3.89764512;
+  char nilaiAbjad = 'A';
+  boolean tampan = true;
+ }
+}
